58be210e2c5d22f7ddc9caaab9d37d500eed5c52
BlueNeet <w1547220803@gmail.com>
2020-04-22 20:56:11 +0800

    添加了三目运算的功能

--- src/com/juice/test/TernaryOperatorTest.java
+++ src/com/juice/test/TernaryOperatorTest.java
@@ -1,5 +1,6 @@
 package com.juice.test;
 
+import com.juice.utils.CallFunction;
 import com.juice.utils.TernaryOperator;
 import com.juice.utils.AssignVar;
 import com.juice.utils.Variable;
@@ -9,7 +10,12 @@ import org.junit.Test;
 public class TernaryOperatorTest {
     @Test
     public void ternaryOperatorTest() {
-        String s = "如果 钱包 大于 十 则 看看 “钱太多了” 否则 看看 “我穷死了”";
+        CallFunction.CallFunction("整数 气温 等于 十");
+        int qw = Variable.getVar("气温");
+        Assert.assertEquals(10, qw);
+        String s = "如果 气温 大于 八 则 气温 减少 四 否则 气温 减少 三";
         TernaryOperator.ternaryOperator(s);
+        int qw1=Variable.getVar("气温");
+        Assert.assertEquals(6, qw1);
     }
 }
--- src/com/juice/utils/TernaryOperator.java
+++ src/com/juice/utils/TernaryOperator.java
@@ -1,10 +1,5 @@
 package com.juice.utils;
 
-
-
-import com.sun.org.apache.xpath.internal.objects.XString;
-import sun.awt.windows.WPrinterJob;
-
 public class TernaryOperator {
     /**
      *将读入字符串拆成3份进行三目运算
@@ -12,14 +7,13 @@ public class TernaryOperator {
      */
     public static void ternaryOperator(String str){
         String[] Baa=str.split("则");
-        String[] ifu=Baa[0].trim().split("如果 ");
-        String ifuu=ifu[1];
+        String[] ifs=Baa[0].trim().split("如果 ");
+        String ifc=ifs[1];
         String[] zes=Baa[1].trim().split(" 否");
         String ze=zes[0];
         String fz=Baa[2].trim();
         System.out.println("....................");
-
-        if(JudgeOperator.judgeOperator(ifuu)){
+        if(JudgeOperator.judgeOperator(ifc)){
             CallFunction.CallFunction(ze);
         }
         else{
