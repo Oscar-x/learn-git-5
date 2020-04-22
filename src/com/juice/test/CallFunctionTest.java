d0afc37c2735356f90f981589baee07f48f9c8d0
soreak <1498985670@qq.com>
2020-04-22 19:23:40 +0800

    add CallFunction and it's test

--- src/com/juice/test/CallFunctionTest.java
+++ src/com/juice/test/CallFunctionTest.java
@@ -1,6 +1,7 @@
 package com.juice.test;
 
 import com.juice.utils.CallFunction;
+import com.juice.utils.Variable;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -8,9 +9,11 @@ public class CallFunctionTest {
     @Test
     public void CallFunctionTest() {
         CallFunction.CallFunction("整数 气温 等于 十");
-
+        int temp= Variable.getVar("气温");
+        Assert.assertEquals(10,temp);
         CallFunction.CallFunction("气温 减少 三");
-
+        int temp1=Variable.getVar("气温");
+        Assert.assertEquals(7,temp1);
         /*CallFunction.CallFunction("看看 气温");*/
 
         /*CallFunction.CallFunction("如果 气温 大于 八 则 看看 “你好，世界” 否则 看看 “冻死我了”");*/
--- src/com/juice/utils/CallFunction.java
+++ src/com/juice/utils/CallFunction.java
@@ -2,25 +2,27 @@ package com.juice.utils;
 
 import org.junit.Test;
 
+/**
+ * 识别语句关键字，调用对应方法
+ */
 public class CallFunction {
-    @Test
+    /**
+     * 对传入语句的第一个词进行判断，调用相应类
+     * @param str 传入的字符串
+     */
     public static void CallFunction(String str) {
 
         String[] str1 = str.split("\\s+");
         String varStr = str1[0];
         switch (varStr) {
             case "整数":
-                AssignVar.assignVar(str);
-                System.out.println("初始值设置成功");break;
+                AssignVar.assignVar(str);break;
             case "气温":
-                ManipulateNum.manipulateNum(str);
-                System.out.println("修改成功");break;
+                ManipulateNum.manipulateNum(str);break;
             case "看看":
-                /*PrintOut.printOut(str);
-                System.out.println("输出成功");break;/
+                /*PrintOut.printOut(str);break;/
             case "如果":
-               /* TernaryOperator.ternaryOperator(str);
-                System.out.println("正在进行对比");break;*/
+               /* TernaryOperator.ternaryOperator(str);break;*/
         }
     }
 }
