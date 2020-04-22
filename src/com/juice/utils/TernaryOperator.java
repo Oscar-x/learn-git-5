f39a6fbcae2f497a4b604ee9edbe7faf70d94982
BlueNeet <w1547220803@gmail.com>
2020-04-22 21:53:53 +0800

    添加了三目运算的功能

--- src/com/juice/utils/TernaryOperator.java
+++ src/com/juice/utils/TernaryOperator.java
@@ -1,23 +1,26 @@
 package com.juice.utils;
 
+/**
+ * 解析条件判断句子 “如果...则...否则...” 的类
+ */
 public class TernaryOperator {
     /**
-     *将读入字符串拆成3份进行三目运算
+     *分解完String后分别调用 JudgeOperator.judgeOperator()和CallFunction.CallFunction()来进行判断与执行相应的语句
      *
+     * @param str “如果...则...否则...” 句式的String
      */
     public static void ternaryOperator(String str){
         String[] Baa=str.split("则");
-        String[] ifs=Baa[0].trim().split("如果 ");
-        String ifc=ifs[1];
-        String[] zes=Baa[1].trim().split(" 否");
-        String ze=zes[0];
-        String fz=Baa[2].trim();
-        System.out.println("....................");
-        if(JudgeOperator.judgeOperator(ifc)){
-            CallFunction.CallFunction(ze);
+        String[] str1=Baa[0].trim().split("如果 ");
+        String str2=str1[1];
+        String[] str3=Baa[1].trim().split(" 否");
+        String str4=str3[0];
+        String str5=Baa[2].trim();
+        if(JudgeOperator.judgeOperator(str2)){
+            CallFunction.CallFunction(str4);
         }
         else{
-            CallFunction.CallFunction(fz);
+            CallFunction.CallFunction(str5);
         }
     }
 }
