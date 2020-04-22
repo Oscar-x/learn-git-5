cbf2594be29a8f51adae0d2caf490eb5abf0f08e
soreak <1498985670@qq.com>
2020-04-22 19:28:29 +0800

    add CallFunction and it's test

--- src/com/juice/test/CallFunctionTest.java
+++ src/com/juice/test/CallFunctionTest.java
@@ -9,11 +9,11 @@ public class CallFunctionTest {
     @Test
     public void CallFunctionTest() {
         CallFunction.CallFunction("整数 气温 等于 十");
-        int temp= Variable.getVar("气温");
-        Assert.assertEquals(10,temp);
+        int temp = Variable.getVar("气温");
+        Assert.assertEquals(10, temp);
         CallFunction.CallFunction("气温 减少 三");
-        int temp1=Variable.getVar("气温");
-        Assert.assertEquals(7,temp1);
+        int temp1 = Variable.getVar("气温");
+        Assert.assertEquals(7, temp1);
         /*CallFunction.CallFunction("看看 气温");*/
 
         /*CallFunction.CallFunction("如果 气温 大于 八 则 看看 “你好，世界” 否则 看看 “冻死我了”");*/
--- src/com/juice/utils/CallFunction.java
+++ src/com/juice/utils/CallFunction.java
@@ -1,13 +1,12 @@
 package com.juice.utils;
 
-import org.junit.Test;
-
 /**
  * 识别语句关键字，调用对应方法
  */
 public class CallFunction {
     /**
      * 对传入语句的第一个词进行判断，调用相应类
+     *
      * @param str 传入的字符串
      */
     public static void CallFunction(String str) {
@@ -16,9 +15,11 @@ public class CallFunction {
         String varStr = str1[0];
         switch (varStr) {
             case "整数":
-                AssignVar.assignVar(str);break;
+                AssignVar.assignVar(str);
+                break;
             case "气温":
-                ManipulateNum.manipulateNum(str);break;
+                ManipulateNum.manipulateNum(str);
+                break;
             case "看看":
                 /*PrintOut.printOut(str);break;/
             case "如果":
