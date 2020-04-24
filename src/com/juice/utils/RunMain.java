6f7a448882a3aefd76d73d4ba3e5ac014b7070e9
Oscar-x <2796769363@qq.com>
2020-04-24 17:00:45 +0800

    添加了方法注释

--- src/com/juice/utils/RunMain.java
+++ src/com/juice/utils/RunMain.java
@@ -2,10 +2,13 @@ package com.juice.utils;
 
 import java.util.Scanner;
 /**
- *主类，循环输入，调用CallFunction
+ *主类
  */
 public class RunMain {
-
+    /**
+     *循环读取用户输入并调用callFunction
+     * @param args 控制台输入字符串，例如“整数 气温 等于 十”
+     */
     public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            while(true){
